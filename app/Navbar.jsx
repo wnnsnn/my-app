3265db3387def4c74d16b66075b0585c12478c97
WinsonBaring <winsonbaring10@gmail.com>
2023-03-05 20:52:44 +0800

    Initial Start up
    Next Js with Tailwind CSS Experimental Project
    
    I will add also experiment mongodb soon :)

---
+++ app/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <div className='bg-white'>I am shit</div>
+    )
+}
+
+export default navbar
---
+++ app/todos/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const page = () => {
+    return (
+        <>
+        <Link href='/'>Click to back at the homepage</Link>
+        <div>page 1</div>
+        </>
+    )
+}
+
+export default page
